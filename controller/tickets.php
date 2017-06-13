9e6ad0c3f051924d209063fd4a60dd6ee0408a26
Ruben <ruben@servibyte.com>
2017-06-13 16:00:12 +0100

    Corrección de la query de Facebook y control de no tener resultados

--- controller/tickets.php
+++ controller/tickets.php
@@ -448,22 +448,20 @@ if (isLoggedIn()) {
             break;
         case 'facebook':
             if ($_SESSION['cliente'] == 'admin') $result = $database->query('SELECT `idlocal`, `fbid`, `name`, `email`, `link`, `username` FROM `facebook`');
-            else $result = $database->query('SELECT `idlocal`, `fbid`, `name`, `email`, `link`, `username` FROM `facebook` WHERE idlocal="'.$_SESSION['local'].'"');
-            
-           
-            $out = array();
-            while ($aux = $result->fetch_assoc()) {
-                array_shift($aux);
-                $aux = array_merge(array('Local'=>  strstr($aux['username'], '_', TRUE)), $aux);
-                $aux['username']= substr(strstr($aux['username'], '_'), 1);
-                $out[]=$aux;
+            else $result = $database->query('SELECT `idlocal`, `fbid`, `name`, `email`, `link`, `username` FROM `facebook` WHERE idlocal= (SELECT id FROM `locales` WHERE nombre="'.$_SESSION['local'].'")');
+            if ($result->num_rows > 0) {           
+                $out = array();
+                while ($aux = $result->fetch_assoc()) {
+                    array_shift($aux);
+                    $aux = array_merge(array('Local'=>  strstr($aux['username'], '_', TRUE)), $aux);
+                    $aux['username']= substr(strstr($aux['username'], '_'), 1);
+                    $out[]=$aux;
+                }
+                $menu = array();
+                foreach ($out as $value) foreach($value as $item) $menu[]=$item;
+                $smarty->assign('cols', implode(',', array_keys($out[0])));
+                $smarty->assign('facebook', $menu);
             }
-            $menu = array();
-            foreach ($out as $value) foreach($value as $item) $menu[]=$item;
-            $smarty->assign('cols', implode(',', array_keys($out[0])));
-            $smarty->assign('facebook', $menu);
-            
-            
             break;
         case 'bloc':
             if (!empty($postparams)) {
