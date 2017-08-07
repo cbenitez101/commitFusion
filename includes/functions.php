783434c9eac46c2c28aab6daf34e247746f6dc28
Christoffer <c.benitez101@gmail.com>
2017-08-07 16:18:00 +0100

    Se me olvido quitar el fileputcontents xD

--- includes/functions.php
+++ includes/functions.php
@@ -488,7 +488,6 @@ function external_crea_ticket() {
                 }   
             }  
         }    
-        file_put_contents('aquapark', print_r($_POST, true));
         echo (($_POST['password'] == 'usuario')?(($_POST['servername'] == "coronablanca")?"user=$usuario&full=1&identificador=".$_POST['identificador']."&precio=".$_POST['precio']."&duracion=".$_POST['duracion']."&hotspot=coronablanca":(($_POST['servername'] == "AquaparkLanzarote&fecha=".date("d-m-y"))?"user=$usuario&precio=".$_POST['precio']."&duracion=".$_POST['duracion']."&hotspot=AquaparkLanzarote&fecha":"user=$usuario")):"user=$usuario&pass=$contrasena");
         die();
     }
