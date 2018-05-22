7540d985480dd4ad9d45e4fd7162ef65656604b9
Christo <c.benitez101@gmail.com>
2018-05-22 10:00:24 +0100

    Correccion funcionalidad acciones: se anade apikey

--- scripts/includes/mantenimiento.js
+++ scripts/includes/mantenimiento.js
@@ -153,7 +153,7 @@ function table_actions(table, action){
     $.ajax({
         url: '/reparar_radacct',
         type: 'POST',
-        data:{tabla:table, accion:action}
+        data:{tabla:table, accion:action, api: '943756eb7841efcc43b7cd37d7254c76' }
     }).done(function(data){
         if(data) console.log('Exito');
         else console.log('Error');
