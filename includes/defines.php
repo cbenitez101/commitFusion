50aebbc2cb2b396e95b7be40788b3c7b3f815433
Christo <c.benitez101@gmail.com>
2018-05-29 11:29:41 +0100

    Se anade default_timezone para evitarque se muestre la hora con un ahora de mas

--- includes/defines.php
+++ includes/defines.php
@@ -11,7 +11,7 @@ global $fulldomain;
 $fulldomain=getcwd();
 global $mediapath;
 $mediapath = $fulldomain;
-
+date_default_timezone_set("Atlantic/Canary");
 /**
  * Only these values are allowed in the first part of the URI
  * Otherwise the call is redirected to the mainpage
@@ -29,19 +29,19 @@ $pages = array(
 /**
  * Settings for all pages
  */
-define(SMARTY_DIR, __DIR__.'/../libs/');
+define('SMARTY_DIR', __DIR__.'/../libs/');
 
-
-define(DOMAIN, 'http://'.$_SERVER['SERVER_NAME'].((strstr($_SERVER['SERVER_NAME'], '192.168'))?':8080':''));
+define('APIKEY', md5('sbyte'));
+define('DOMAIN', 'http://'.$_SERVER['SERVER_NAME'].((strstr($_SERVER['SERVER_NAME'], '192.168'))?':8080':''));
 //define(DOMAIN, 'http://localhost:8888');
 
-define(__DBHOST__, 'localhost');
-define(__DBUSER__, 'platformuser');
-define(__DBPASS__, 'rfC79w?3');
-define(__DBNAME__, 'plataforma');
+define('__DBHOST__', 'localhost');
+define('__DBUSER__', 'platformuser');
+define('__DBPASS__', 'rfC79w?3');
+define('__DBNAME__', 'plataforma');
 
-define(__RAHOST__, 'localhost');
-define(__RAUSER__, 'radiususer');
-define(__RAPASS__, 'Pwp+*f2b');
-define(__RANAME__, 'radius');
+define('__RAHOST__', 'localhost');
+define('__RAUSER__', 'radiususer');
+define('__RAPASS__', 'Pwp+*f2b');
+define('__RANAME__', 'radius');
 ?>
